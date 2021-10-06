4106da17d55db8e73889339252e32b9c0d78d55b
yannisf <yannis@ouranos>
2021-10-06 09:27:58 +0300

    Engineering fixes

--- src/main/java/eu/frlab/SchoolCard.java
+++ src/main/java/eu/frlab/SchoolCard.java
@@ -2,8 +2,10 @@ package eu.frlab;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.bridge.SLF4JBridgeHandler;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -14,6 +16,8 @@ public class SchoolCard {
 
     private static final long WAIT_TIME = 1000;
 
+    private static final Logger LOG = LoggerFactory.getLogger(SchoolCard.class);
+
     public static void main(String[] args) throws Exception {
         String home = System.getenv("HOME");
         Path gsisPath = Paths.get(home + "/.gsis");
@@ -22,21 +26,33 @@ public class SchoolCard {
         argCheck(args);
         String[] kid = Files.readAllLines(Paths.get(args[0])).get(0).split(":");
         infoCheck(kid);
+        LOG.info("Preparing COVID school card for [{}]", kid[0]);
         SchoolCard schoolCard = new SchoolCard();
         schoolCard.execute(gsis, kid);
         System.exit(0);
     }
 
+    private WebDriver getDriver() {
+        if (System.getenv("CHROME_DRIVER") != null)
+            System.setProperty("webdriver.chrome.driver", System.getenv("CHROME_DRIVER"));
+
+        System.setProperty("webdriver.chrome.silentOutput", "true");
+
+        SLF4JBridgeHandler.removeHandlersForRootLogger();
+        SLF4JBridgeHandler.install();
+        return new ChromeDriver();
+    }
+
     private static void gsisCheck(Path gsisPath) {
         if (!(Files.exists(gsisPath) && Files.isReadable(gsisPath))) {
-            System.out.println("A file, named '.gsis', with GSIS (TaxisNet) credentials in the format 'username:password' does not exist in your home directory.");
+            LOG.error("A file, named '.gsis', with GSIS (TaxisNet) credentials in the format 'username:password' does not exist in your home directory.");
             System.exit(1);
         }
     }
 
     private static void argCheck(String[] args) {
         if (args.length != 1) {
-            System.out.println("Exactly one parameter is expected, with the path of the file containing the kid's information in the format 'NAME:SURNAME:FATHER_NAME:MOTHER_NAME:DAY_OF_BIRTH:MONTH_OF_BIRTH:YEAR_OF_BIRTH:AMKA'.");
+            LOG.error("Exactly one parameter is expected, with the path of the file containing the kid's information in the format 'NAME:SURNAME:FATHER_NAME:MOTHER_NAME:DAY_OF_BIRTH:MONTH_OF_BIRTH:YEAR_OF_BIRTH:AMKA'.");
             System.exit(1);
         }
     }
@@ -49,37 +65,44 @@ public class SchoolCard {
     }
 
     private void execute(String[] gsis, String[] kid) throws Exception {
+
+        WebDriver driver = getDriver();
+        LOG.info("Open gov.gr COVID19 School Card web page");
         String url = "https://covid19-self-test.services.gov.gr/templates/COVID19-SCHOOL-CARD2/create/";
-
-        if (System.getenv("CHROME_DRIVER") != null)
-            System.setProperty("webdriver.chrome.driver", System.getenv("CHROME_DRIVER"));
-
-        WebDriver driver = new ChromeDriver();
         driver.get(url);
         Thread.sleep(WAIT_TIME);
 
+        LOG.info("Accept cookies");
         driver.findElement(By.xpath("//button[span[text()[contains(.,'Αποδοχή')]]]")).click();
         Thread.sleep(WAIT_TIME);
 
+        LOG.info("Login on service");
         driver.findElement(By.cssSelector("a[href='/api/login/']")).click();
         Thread.sleep(WAIT_TIME);
 
+        LOG.info("Select login authenticator (GSIS)");
         driver.findElement(By.xpath("//button[span[text()[contains(.,'ΓΓΠΣΔΔ')]]]")).click();
         Thread.sleep(WAIT_TIME);
 
+        LOG.info("Enter credentials");
         driver.findElement(By.name("j_username")).sendKeys(gsis[0]);
         driver.findElement(By.name("j_password")).sendKeys(gsis[1]);
         driver.findElement(By.id("btn-login-submit")).click();
         Thread.sleep(WAIT_TIME);
 
+        LOG.info("Review GSIS data");
         try {
             driver.findElement(By.id("btn-submit")).click();
             Thread.sleep(WAIT_TIME);
-        } catch (Exception ignored) { }
+        } catch (Exception ignored) {
+            LOG.info("Review GSIS data (skipped)");
+        }
 
+        LOG.info("Review personal information");
         driver.findElement(By.cssSelector("button[label='Συνέχεια']")).click();
         Thread.sleep(WAIT_TIME);
 
+        LOG.info("Enter kids personal information");
         driver.findElement(By.name("child_firstname")).sendKeys(kid[0]);
         driver.findElement(By.name("child_surname")).sendKeys(kid[1]);
         driver.findElement(By.name("child_fathername")).sendKeys(kid[2]);
@@ -88,21 +111,22 @@ public class SchoolCard {
         driver.findElement(By.name("child_birth_date-month")).sendKeys(kid[5]);
         driver.findElement(By.name("child_birth_date-year")).sendKeys(kid[6]);
         driver.findElement(By.cssSelector("form")).submit();
-
         Thread.sleep(WAIT_TIME);
+
+        LOG.info("Enter kids COVID test information");
         LocalDate date = LocalDate.now();
         driver.findElement(By.name("amka")).sendKeys(kid[7]);
         driver.findElement(By.name("test_date-day")).sendKeys(String.valueOf(date.getDayOfMonth()));
         driver.findElement(By.name("test_date-month")).sendKeys(String.valueOf(date.getMonthValue()));
         driver.findElement(By.name("test_date-year")).sendKeys(String.valueOf(date.getYear()));
         driver.findElement(By.cssSelector("#mui-component-select-test_result")).click();
-
-        Thread.sleep(WAIT_TIME);
         driver.findElement(By.cssSelector("#menu-test_result > div.MuiPaper-root.MuiMenu-paper.MuiPopover-paper.MuiPaper-elevation8.MuiPaper-rounded > ul > li:nth-child(1)")).click();
 
+        LOG.info("Submit form");
         Thread.sleep(WAIT_TIME);
         driver.findElement(By.cssSelector("form")).submit();
 
+        LOG.info("Request print");
         Thread.sleep(WAIT_TIME);
         driver.findElement(By.cssSelector("button[label='Εκτύπωση']")).click();
     }
